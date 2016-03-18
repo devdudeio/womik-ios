d7a2193ce2eafd2946866a1c1bd8835e94f04e85
Robert Lech <robert.lech@me.com>
2016-03-18 15:18:26 +0100

    picker added

--- client/templates/eventlist/eventlist.js
+++ client/templates/eventlist/eventlist.js
@@ -1,17 +1,47 @@
 Template.eventlist.helpers({
     events: function () {
-        return Events.find({hidden:false},{sort: {begin: -1}});
+        return Events.find({hidden: false}, {sort: {begin: 1}});
     }
 });
-
 Template.eventlist.rendered = function () {
     var $$ = Dom7;
-    var myApp = new Framework7();
-    //Meteor.call("console", Session.get("device"));
+
+    var a = new Framework7();
+
+
+    var myPicker = a.picker({
+        input: '#picker-text',
+        rotateEffect: true,
+        toolbarCloseText:'Auswählen',
+        cols: [
+            {
+                textAlign: 'center',
+                values: ['1', '2', '3'],
+                displayValues: ['Apple', 'Orange', 'Bananna']
+            }
+        ],
+        onChange: function(p,v){
+            Session.set("orderByCityID", v);
+        },
+        onClose: function(p){
+            alert(Session.get("orderByCityID")+ " itemstotal: " + Events.find().count());
+        }
+    });
+
+
 };
 
 Template.eventlist.onCreated(function () {
 
 
     //TODO template subscription ready stuff laterszzzZZZZz....r
+});
+
+
+Template.eventlist.events({
+    'click .center .sliding': function () {
+        Meteor.call("console", "click");
+
+
+    }
 });
