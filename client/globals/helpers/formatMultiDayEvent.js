d006228e0f2d768bb770509fbf92451db5965347
Robert Lech <robert.lech@me.com>
2016-03-20 13:51:03 +0100

    updated

--- client/globals/helpers/formatMultiDayEvent.js
+++ client/globals/helpers/formatMultiDayEvent.js
@@ -4,7 +4,6 @@
 Template.registerHelper("formatMultiDayEvent", function (b,e) {
     let temp = moment(b).format("dddd, DD.MM.YYYY HH:mm ");
     temp = temp + moment(e).format("- dddd, DD.MM.YYYY HH:mm");
-    Meteor.call("console", M)
     return temp;
 });
 
