ee389bbd7785208ddd6b9f2fef94147247ae1387
Robert Lech <robert.lech@me.com>
2016-03-20 11:55:35 +0100

    updated areas

--- client/templates/eventlist/eventlist.js
+++ client/templates/eventlist/eventlist.js
@@ -7,16 +7,24 @@ Template.eventlist.helpers({
 });
 Template.eventlist.rendered = function () {
     var $$ = Dom7;
-
     var a = new Framework7();
 
+    let areas = Areas.find({}, {sort: {name: 1}});
+    let area_names = [];
+    let area_ids = [];
 
-    //var cities = Events.find();
+    areas.forEach(function (e) {
+        area_ids.push(e._id);
+        area_names.push(e.name);
+    });
+
+    Meteor.call("console", area_ids);
+    Meteor.call("console", area_names);
 
     var myPicker = a.picker({
         input: '#picker-text',
         rotateEffect: true,
-        toolbarCloseText:'Auswählen',
+        toolbarCloseText: 'Auswählen',
         cols: [
             {
                 textAlign: 'center',
@@ -24,11 +32,11 @@ Template.eventlist.rendered = function () {
                 displayValues: ['Dresden', 'Leipzig', 'Chemnitz']
             }
         ],
-        onChange: function(p,v){
+        onChange: function (p, v) {
             Session.set("orderByCityID", v);
         },
-        onClose: function(p){
-            alert(Session.get("orderByCityID")+ " itemstotal: " + Events.find().count());
+        onClose: function (p) {
+            alert(Session.get("orderByCityID") + " itemstotal: " + Events.find().count());
         }
     });
 
